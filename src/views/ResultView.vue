b6e6069b8d58779e3180eb95ff64fe3788794132
juaniwk3 <86119976+JuanIWK3@users.noreply.github.com>
2025-01-13 16:19:16 -0300

    vue init

---
+++ src/views/ResultView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useGameStore } from '@/stores/game-store';
+
+const store = useGameStore()
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col items-center justify-center">
+    <h1 class="font-bold text-xl">Result</h1>
+    <p>Points scored: {{ store.points }}</p>
+
+    <router-link to="/" class="mt-4 rounded border py-2 px-4">Play again</router-link>
+  </div>
+</template>
