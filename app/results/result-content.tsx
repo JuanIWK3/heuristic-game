8b13e3a1403fe927e50f64abb84e2a783fa0ec66
juaniwk3 <86119976+JuanIWK3@users.noreply.github.com>
2025-01-13 19:12:13 -0300

    Refactor results page to use ResultsContent component and implement Suspense for loading state

---
+++ app/results/result-content.tsx
@@ -0,0 +1,59 @@
+import { Button } from '@/components/ui/button';
+import { gameContext } from '@/context/game';
+import { Link, MoveLeft } from 'lucide-react';
+import { useSearchParams } from 'next/navigation';
+import { use } from 'react';
+
+export function ResultsContent() {
+  const { sites } = use(gameContext);
+  const params = useSearchParams();
+  const points = Number(params.get('points'));
+
+  return (
+    <div className="min-h-screen gap-8 p-8 flex flex-col items-center justify-center">
+      <Link href="/">
+        <Button variant={'outline'} className="text-blue-500">
+          <MoveLeft /> Play again
+        </Button>
+      </Link>
+      <h1 className="font-bold text-2xl">Results</h1>
+      <p>
+        You scored <Button variant={'outline'}>{points}</Button> points
+      </p>
+
+      <div className="w-full max-w-sm">
+        <h1 className="text-center font-bold mb-4">Answers</h1>
+        <ul>
+          {sites.map((site) => (
+            <div className="flex flex-col gap-4" key={site.name}>
+              {site.problems.map((problem) => (
+                <div
+                  className={`flex border flex-col gap-4 w-full p-4 rounded ${
+                    problem.userAnswer === problem.answer
+                      ? 'border-green-500'
+                      : 'border-red-500'
+                  }`}
+                  key={problem.description}
+                >
+                  <h2>{problem.description}</h2>
+                  <div className="flex justify-between w-full">
+                    <p>Yours</p>
+                    <p>Correct</p>
+                  </div>
+                  <div className="flex justify-between w-full">
+                    <p>
+                      {problem.userAnswer
+                        ? problem.userAnswer
+                        : 'You did not answer'}
+                    </p>
+                    <h2>{problem.answer}</h2>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
